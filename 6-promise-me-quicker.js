5aca89330b0357b8c8751a5b6f836a215f065888
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2018-10-31 13:31:49 -0600

    finished number 6

---
+++ 6-promise-me-quicker.js
@@ -0,0 +1,17 @@
+var catchPromise = new Promise(function (fulfill, reject) {
+    reject(new Error('I am an error'));
+});
+
+catchPromise.catch(function(error) {
+    console.log(error.message);
+});
+
+var resolvePromise = Promise.resolve('Hello I am resolved');
+
+resolvePromise.then(console.log, null);
+
+var rejectPromise = Promise.reject(new Error('Hello I am rejected'));
+
+rejectPromise.then(null, function(error) {
+    console.log(error.message);
+});
