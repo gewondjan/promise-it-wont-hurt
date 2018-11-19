b7712d4aab91a9c57618a6e1bcde28c1dd555e63
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2018-11-19 13:55:30 -0700

    finished 9

--- 9-Throw-an-Error.js
+++ 9-Throw-an-Error.js
@@ -1,7 +1,23 @@
-function returnNumber(x) {
-    return x;
+function handleError(error) {
+    console.log(error.message);
 }
 
-// Promise.resolve(3).then(returnNumber).then(console.log);
+//For fun
+//var json = `{"name": "Ryan"}`;
 
-Promise.resolve(34).then(returnNumber).then(console.log)
+Promise.resolve(process.argv[2]).then(JSON.parse).then(console.log).then(null, handleError);
+
+//ANOTHER WAY TO DO IT
+// function parsePromise(json) {
+//     return new Promise(function (fulfill, reject) {
+//         try {
+//             var ob = JSON.parse(json);
+//             fulfill(ob);
+//         } catch (e) {
+//             reject(e); //This passes an error to the reject method. (try catch block throws error).
+//         }
+
+//     });
+// }
+
+// parsePromise(process.argv[2]).then(null, handleError);
